0ec42cdd52e058551a8d0622a51c9707f682bb1b
Wes Koerber <wes@weskoerber.com>
2024-02-11 12:50:44 -0500

    chore: add lib artifacts

--- build.zig
+++ build.zig
@@ -4,7 +4,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addModule("kewpie", .{
+    const lib = b.addStaticLibrary(.{
+        .name = "kewpie",
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = .{ .path = "./lib/root.zig" },
+    });
+
+    const lib_shared = b.addSharedLibrary(.{
+        .name = "kewpie",
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = .{ .path = "./lib/root.zig" },
+        .version = std.SemanticVersion{
+            .major = 0,
+            .minor = 1,
+            .patch = 0,
+        },
+    });
+
+    b.installArtifact(lib);
+    b.installArtifact(lib_shared);
+
+    const mod = b.addModule("kewpie", .{
         .target = target,
         .optimize = optimize,
         .root_source_file = .{ .path = "./lib/root.zig" },
@@ -18,7 +40,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = .{ .path = "./test/root.zig" },
     });
 
-    tests.root_module.addImport("kewpie", lib);
+    tests.root_module.addImport("kewpie", mod);
 
     const run_tests = b.addRunArtifact(tests);
     test_step.dependOn(&run_tests.step);
